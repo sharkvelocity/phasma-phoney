730155a940daedf14ac4ddad70271de175ddbc30
sharkvelocity <71565691+sharkvelocity@users.noreply.github.com>
2025-06-21 11:06:32 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,32 +2,8 @@ const CACHE_NAME = 'phasma-phoney-v1';
 const urlsToCache = [
   './',
   './index.html',
-  './styles.css',
   './logo.png',
-  './game.js',
-  './mechanics.js',
-  './statistics.js',
-  './narrator.js',
-  './dialogueEngine.js',
-  './ghosts.js',
-  './meta.js',
-  './turnSystem.js',
-  './multiRun.js',
-  './commandEngine.js',
-  './commandParser.js',
-  './ghostManager.js',
-  './ghostTalk.js',
-  './evidence.js',
-  './dynamicGhost.js',
-  './map.js',
-  './miniMap.js',
-  './van.js',
-  './saveSystem.js',
-  './inventory.js',
-  './journal.js',
-  './dialogue.js',
-  './ai_narrator.js',
-  './succubus.js'
+  './Title.png',
 ];
 
 self.addEventListener('install', event => {
